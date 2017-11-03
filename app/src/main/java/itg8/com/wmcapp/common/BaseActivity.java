d26e17039d31af84f8af2fffe143a826ad24d7d2
AndroidItg8 <itechgalaxy.app@gmail.com>
2017-11-03 12:28:19 +0530

    yes

--- app/src/main/java/itg8/com/wmcapp/common/BaseActivity.java
+++ app/src/main/java/itg8/com/wmcapp/common/BaseActivity.java
@@ -1,18 +1,29 @@
 package itg8.com.wmcapp.common;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+
+import java.util.Locale;
 
 import itg8.com.wmcapp.R;
+import itg8.com.wmcapp.home.HomeActivity;
+import itg8.com.wmcapp.prabhag.PrabhagFragment;
 
 /**
  * Created by Android itg 8 on 11/2/2017.
  */
 
 public class BaseActivity  extends AppCompatActivity{
+
+    private FragmentTransaction ft;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,7 +32,22 @@ public class BaseActivity  extends AppCompatActivity{
 
 
     public void callFragment(Fragment fragment) {
-        FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction().replace(R.id.frame_container, fragment).addToBackStack(fragment.getClass().getSimpleName()).commit();
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+        ft.replace(R.id.frame_container,fragment);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void setLocale(String lang,String countryCode) {
+        Locale myLocale = new Locale(lang,countryCode);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, HomeActivity.class);
+        startActivity(refresh);
+        finish();
     }
 }
--- app/src/main/java/itg8/com/wmcapp/home/HomeActivity.java
+++ app/src/main/java/itg8/com/wmcapp/home/HomeActivity.java
@@ -13,14 +13,35 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import itg8.com.wmcapp.R;
 import itg8.com.wmcapp.common.BaseActivity;
+import itg8.com.wmcapp.common.CallType;
+import itg8.com.wmcapp.common.CommonCallback;
+import itg8.com.wmcapp.common.CustomDialogFragment;
 import itg8.com.wmcapp.complaint.ComplaintFragment;
+import itg8.com.wmcapp.prabhag.PrabhagFragment;
+import itg8.com.wmcapp.prabhag.WardMemberFragment;
+import itg8.com.wmcapp.prabhag.dummy.DummyContent;
+import itg8.com.wmcapp.prabhag.model.ContactModel;
 import itg8.com.wmcapp.signup.SignUpFragment;
 import itg8.com.wmcapp.torisum.TorisumFragment;
 
+import static itg8.com.wmcapp.common.CallType.PRABHAG;
+import static itg8.com.wmcapp.common.CallType.WARD;
+import static itg8.com.wmcapp.common.CallType.WARD_MEMBER;
+
 public class HomeActivity extends BaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,CommonCallback.OnImagePickListener,
+        CustomDialogFragment.DialogItemClickListener,
+        PrabhagFragment.OnListFragmentInteractionListener {
+
+    private CallType isFrom;
+    private ComplaintFragment fragment;
+    CommonCallback.OnDialogClickListner listner;
+    String[] items = {"Pick From Camera", "Pick From File"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +69,7 @@ public class HomeActivity extends BaseActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -84,49 +97,117 @@ public class HomeActivity extends BaseActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        Fragment fragment= null;
-       switch (item.getItemId())
-       {
-           case R.id.nav_notice_board:
-               break;
-           case R.id.nav_complaint:
-               fragment = ComplaintFragment.newInstance("","");
-               break;
-           case R.id.nav_tourism:
-               fragment = TorisumFragment.newInstance("","");
-               break;
-           case R.id.nav_news:
-               break;
-           case R.id.nav_ward:
-               break;
-           case R.id.nav_praphag:
-               break;
-           case R.id.nav_ward_member:
-               break;
-           case R.id.nav_registration:
-               break;
-           case R.id.nav_profile:
-               fragment = ComplaintFragment.newInstance("","");
-               break;
-           case R.id.nav_forget_pswd:
-               break;
-           case R.id.nav_change_password:
-               break;
-           case R.id.nav_suggestion:
-               break;
-           case R.id.nav_feedback:
-               break;
-           case R.id.nav_emgs_no:
-               break;
-           case R.id.nav_contact:
-               fragment = SignUpFragment.newInstance("","");
-               break;
-       }
-       if(fragment!= null) {
-          callFragment(fragment);
-       }
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.nav_notice_board:
+                break;
+            case R.id.nav_complaint:
+                fragment = ComplaintFragment.newInstance("", "");
+                break;
+            case R.id.nav_tourism:
+                fragment = TorisumFragment.newInstance("", "");
+                break;
+            case R.id.nav_news:
+                break;
+            case R.id.nav_ward:
+                break;
+            case R.id.nav_praphag:
+
+                break;
+            case R.id.nav_ward_member:
+                break;
+            case R.id.nav_registration:
+                break;
+            case R.id.nav_profile:
+                fragment = ComplaintFragment.newInstance("", "");
+                break;
+            case R.id.nav_forget_pswd:
+                break;
+            case R.id.nav_change_password:
+                break;
+            case R.id.nav_suggestion:
+                break;
+            case R.id.nav_feedback:
+                break;
+            case R.id.nav_emgs_no:
+                break;
+            case R.id.nav_contact:
+                fragment = SignUpFragment.newInstance("", "");
+                break;
+        }
+        if (fragment != null) {
+            callFragment(fragment);
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    private void showPrabhag() {
+        isFrom = PRABHAG;
+        callFragment(PrabhagFragment.newInstance(1));
+    }
+
+    private void showWard() {
+        isFrom = WARD;
+        callFragment(PrabhagFragment.newInstance(1));
+    }
+
+    private void showWardMemberDetail() {
+        isFrom=WARD_MEMBER;
+        callFragment( WardMemberFragment.newInstance(getDummyContacts()));
+    }
+
+
+
+    public List<ContactModel> getDummyContacts() {
+        List<ContactModel> models=new ArrayList<>();
+        models.add(new ContactModel());
+        models.add(new ContactModel());
+        return models;
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+
+            if (isFrom == WARD)
+                isFrom = PRABHAG;
+            else if (isFrom == PRABHAG)
+                isFrom = null;
+            else if (isFrom == WARD_MEMBER)
+                isFrom = WARD;
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public void onImagePickClick() {
+        CustomDialogFragment.newInstance(items).show(getSupportFragmentManager(), CustomDialogFragment.TAG);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        if (position == 0) {
+            listner.onOpenCamera();
+        } else if (position == 1) {
+            listner.onSelectFromFileManager();
+        }
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        if (isFrom == PRABHAG) {
+            showWard();
+        }else if(isFrom == WARD){
+            showWardMemberDetail();
+        }
+    }
 }
+
